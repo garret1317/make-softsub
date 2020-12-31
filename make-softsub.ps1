e3501eb1eab54b42d80c76d2e9880a8ef1acfeab
garret1317 <76261416+garret1317@users.noreply.github.com>
2020-12-31 11:28:45 +0000

    make whitespace better

--- make-softsub.ps1
+++ make-softsub.ps1
@@ -3,11 +3,9 @@ $video=$args[0]
 $subs=$args[1]
 Write-Host "making images to OCR"
 VideoSubFinderWXW --clear_dirs --run_search --create_cleared_text_images --top_video_image_percent_end=0.2 -i "$video" -o . | Out-Null
-
-
 Write-Host "Images done"
-Write-Host "Starting OCR"
 
+Write-Host "Starting OCR"
 Get-Childitem -Path "TXTImages/" -file |
 Foreach-Object {
 $ye=$_.Name # if you put it in a variable it magically starts working. can't think of a good name
@@ -16,4 +14,5 @@ tesseract -l eng "TXTImages/$ye" "TXTResults/$ye"
 
 Write-Host "making subs"
 VideoSubFinderWXW -o . --create_sub_from_txt_results="$subs" | Out-Null
+
 Write-Host "done!"
