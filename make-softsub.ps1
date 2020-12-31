69318b7ac4af5fa2b0440b44c8c033f20e8f4637
garret <garret@airmail.cc>
2020-12-31 10:51:13 +0000

    make it work

--- make-softsub.ps1
+++ make-softsub.ps1
@@ -1,17 +1,18 @@
 #!/usr/bin/env pwsh
-
+$video=$args[0]
+$subs=$args[1]
 Write-Host "making images to OCR"
-VideoSubFinderWXW --clear_dirs --run_search --create_cleared_text_images --top_video_image_percent_end=0.2 -i "$Args[0]" -o .
-Write-Host "yeeet"
+VideoSubFinderWXW --clear_dirs --run_search --create_cleared_text_images --top_video_image_percent_end=0.2 -i "$video" -o . | Out-Null
+
 
 Write-Host "Images done"
 Write-Host "Starting OCR"
 
-Get-Childitem -Path "TXTImages/*" -file |
+Get-Childitem -Path "TXTImages/" -file |
 Foreach-Object {
-echo $_.Name
-tesseract -l eng "$_.Name" "TXTResults/$imgout"
+$ye=$_.Name # if you put it in a variable it magically starts working. can't think of a good name
+tesseract -l eng "TXTImages/$ye" "TXTResults/$ye"
 }
 
 Write-Host "making subs"
-VideoSubFinderWXW -o . --create_sub_from_txt_results="$Args[1]"
+VideoSubFinderWXW -o . --create_sub_from_txt_results="$subs" | Out-Null
